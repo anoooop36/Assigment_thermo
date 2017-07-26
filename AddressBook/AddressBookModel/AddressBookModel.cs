e668fa8051fb931e228fce9de09b8be02618904b
Chaudhary <anoop.chaudhary@thermofisher.com>
2017-07-26 16:45:36 +0530

    list updation working + new user button is not working

--- AddressBook/AddressBookModel/AddressBookModel.cs
+++ AddressBook/AddressBookModel/AddressBookModel.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 
-public class AddressBookModel
+public class AddressBookModel : INotifyPropertyChanged
 {
-    
+
+    private string name;
+    private string employeeId;
+    private string phoneNo;
+    private string address;
     public AddressBookModel(string n,string a, string e, string p)
 	{
         Name = n;
@@ -15,10 +19,70 @@ public class AddressBookModel
         ImagePath= @"C:\Users\anoop.chaudhary\Documents\Visual Studio 2017\Projects\AddressBook\AddressBook\bin\Debug\images/download.png";
     }
 
-    public string Name { get; set; }
-    public string Address { get; set; }
-    public string EmployeeId { get; set; }
-    public string PhoneNo { get; set; }
-    public string ImagePath { get; set; }
+    public string Name {
+        get {
+            return name;
+        }
+        set {
+            name = value;
+            NotifyPropertyChanged("Name");
+        }
+    }
+    public string Address {
+        get
+        {
+            return address;
+        }
+        set
+        {
+            address = value;
+            NotifyPropertyChanged("Address");
+        }
+    }
+    public string EmployeeId {
+        get
+        {
+            return employeeId;
+           
+        }
+        set
+        {
+            employeeId = value;
+            NotifyPropertyChanged("EmployeeId");
+        }
+    }
+    public string PhoneNo {
+        get
+        {
+            return phoneNo;
+        }
+        set
+        {
+            phoneNo = value;
+            NotifyPropertyChanged("PhoneNo");
+        }
+    }
 
+    private string imagePath;
+    public string ImagePath {
+        get
+        {
+            return imagePath;
+        }
+        set
+        {
+            imagePath = value;
+            NotifyPropertyChanged("ImagePath");
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void NotifyPropertyChanged(string propertyName)
+    {
+        if (PropertyChanged != null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 }
